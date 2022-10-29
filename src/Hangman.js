a8f8c019ce643c6d93f69aeb4becaccd11b8f29d
vitanv <vitanvlad20@gmail.com>
2022-10-29 22:45:38 +0100

    Started working on the hangman react app

---
+++ src/Hangman.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Hangman.css';
+
+export class Hangman extends React.Component{
+  constructor(props){
+    super(props);
+    
+    this.state = {}
+  }
+
+
+  render(){
+    return(
+      <div className='container'>
+        Placeholder
+      </div>
+    )
+  }
+
+}
